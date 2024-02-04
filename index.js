4f33d3cf3d8bd03423b6992fbb988547f5bda8e6
NicoleSG <nicolesantamariagranizo@gmail.com>
2024-02-04 20:32:48 +0000

    added arrays of questions to index.js

--- index.js
+++ index.js
@@ -5,8 +5,59 @@ const generateMarkdown = require("./utils/generateMarkdown");
 
 // array of questions for user
 const questions = [
+        {
+            name: 'projectTitle',
+            message: 'What is the title of your project?',
+            type: 'input'
+        },
+        {
+            name: 'description',
+            message: 'Describe your your project:',
+            type: 'input'
+        },
+        {
+            name: 'install',
+            message:'Does your app require installation?',
+            type: 'input'
+        },
+        {
+            name: 'usage',
+            message: 'What are the instructions to use it?',
+            type: 'editor'
+        },
+        {
+            name: 'license',
+            message: 'What licence do you require?',
+            type: 'list',
+            choices: ['MIT','Apache', 'GPL']
+        },
+        {
+            name: 'contribute',
+            message: 'Please explain any guidelines for other developers if you are open to contributions:',
+            type: 'input'
+        },
+        {
+            name: 'tests',
+            message: 'Are there any tests for your project?',
+            type: 'input'
+        },
+        {
+            name: 'githubID',
+            message: 'Enter your GitHub username:',
+            type: 'input'
+        },
+        {
+            name: 'githubURL',
+            message: 'Please provide the URL to your GitHub profile:',
+            type: 'input'
+        },
+        {
+            name: 'email',
+            message: 'Please enter your email address:',
+            type: 'input'
+        }
+    ];
 
-];
 
 // function to write README file
 function writeToFile(fileName, data) {
